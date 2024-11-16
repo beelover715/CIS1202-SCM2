b1766a1313df68804ac2a7f1954bed55f5f163ce
beelover715 <charles.schneider@sinclair.edu>
2024-11-16 14:46:11 -0500

    Fixed order of operations and variable name issues

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -1,19 +1,26 @@
+// Charles Schneider
+// CIS 1202
+// November 16th, 2024
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
+//function prototype
+float average(int, int, int);
+
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0f;
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	int n1 = 0 , n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
